8ce36601ec1957eeb192fd3fd79d47b7ab4872e0
vikassachan <vikassachan27@gmail.com>
2021-10-06 21:10:31 +0530

    commit

--- Chapter_11/args_with_normal_parameters.py
+++ Chapter_11/args_with_normal_parameters.py
@@ -1,1 +1,9 @@
-#
+#*args with normal parameters
+
+def multiply_nums(*args):
+    multiply = 1
+    for i in args:
+        multiply *=i
+    return multiply
+
+print(multiply_nums(1,2,3,4,5))        
